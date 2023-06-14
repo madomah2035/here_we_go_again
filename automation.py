08057cb8a193b4f8f9b9b461cdf570c98137da71
Musah ADOMAH <kwwamemusah51@gmail.com>
2023-06-14 12:11:09 +0000

    updating changes

--- automation.py
+++ automation.py
@@ -9,5 +9,6 @@ print("Length: ", yt.length)
 
 yd = yt.streams.get_highest_resolution()
 
-yd.download('/Users/sayetech/Desktop/YT_videos')   # the argument of this is the location where you want your
+yd.download('/Users/sayetech/Desktop/YT_videos')  # the argument of this is the location where you want your
 # download to be
+# oops! my code ain't working now
