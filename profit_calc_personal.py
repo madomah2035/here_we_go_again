2c15ac12d16b1dda22b9663dd122028b05640378
Musah ADOMAH <kwwamemusah51@gmail.com>
2023-06-17 07:53:31 +0000

    made changes to profit_calc_personal.py to be able to loop the calculation process

--- profit_calc_personal.py
+++ profit_calc_personal.py
@@ -2,13 +2,18 @@ def profit(num_of_days, initial_capital):
     overall_profit = initial_capital
     daily_interest = 2 / 100
     for num in range(num_of_days):
-        investing_capital = initial_capital - 0.09
+        investing_capital = initial_capital - 0.062499
         daily_gain = investing_capital * daily_interest
         overall_profit += daily_gain
         overall_profit = round(overall_profit, 2)
     print(f"Your compound profit over {num_of_days} days will be: ${overall_profit}")
 
 
-principal = float(input('Enter your capital:$ '))
-days = int(input('How many days are you looking to invest? '))
-profit(num_of_days=days, initial_capital=principal)
+end_of_calc = True
+while end_of_calc:
+    principal = float(input('Enter your capital:$ '))
+    days = int(input('How many days are you looking to invest? '))
+    profit(num_of_days=days, initial_capital=principal)
+    go_again = input("calculate again? (Y/N): ").lower()
+    if go_again != "y":
+        end_of_calc = False
