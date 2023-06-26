2a87a8b579757c2cf6b5e4dfecda6adaef14aaf2
Musah ADOMAH <kwwamemusah51@gmail.com>
2023-06-26 05:02:56 +0000

    update

--- higher_or_lower.py
+++ higher_or_lower.py
@@ -25,10 +25,12 @@ score = 0
 
 not_end_of_game = True
 second_choice = random.choice(database)
+# print(second_choice)
 while not_end_of_game:
     first_choice = second_choice
     # print(first_choice)
     second_choice = random.choice(database)
+    # print(second_choice)
     while first_choice == second_choice:
         second_choice = random.choice(database)
     # print(second_choice)
@@ -45,20 +47,20 @@ while not_end_of_game:
             user = first_choice["followers"]
             if user > second_choice["followers"]:
                 score += 1
-                print(f"You are right! Your score is {score}")
+                print(f"You are right! Your score is {score}.")
             else:
-                print(f"You are wrong! Your final score is {score}")
+                print(f"You are wrong! Your final score is {score}.")
                 not_end_of_game = False
         else:
             user = second_choice["followers"]
             if user > first_choice["followers"]:
                 score += 1
-                print(f"You are right! Your score is {score}")
+                print(f"You are right! Your score is {score}.")
             else:
-                print(f"You are wrong! Your final score is {score}")
+                print(f"Sorry, that's wrong! Your final score is {score}.")
                 not_end_of_game = False
 
 
-    user_choice = input("Who do you think has more followers? A or B: ")
+    user_choice = input("Who do you think has more followers? A or B: ").lower()
 
     get_user_choice(user=user_choice)
